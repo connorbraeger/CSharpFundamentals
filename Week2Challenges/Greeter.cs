a5c4f822d7749befe8e6b08b84401f910dc8bbd3
Connor Braeger <cjbraeger@gmail.com>
2020-11-19 18:28:25 -0500

    week2challenges

---
+++ Week2Challenges/Greeter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Challenges
+{
+    public class Greeter
+    {
+        public void SayHello(string name)//Takes in name, outputs greeting
+        {
+            Console.WriteLine("Hello " + name + "!");
+        }
+        public void SayFarewell(string name)//Takes in name, outputs farewell
+        {
+            Console.WriteLine("Goodbye " + name + "!");
+        }
+        public void TemporalMessage()
+        {
+            DateTime currentTime = DateTime.Now;
+            if (currentTime.Hour < 12)
+            {
+                Console.WriteLine("Good morning!");
+            }else if (currentTime.Hour < 15)
+            {
+                Console.WriteLine("Good Afternoon!");
+            }
+            else if (currentTime.Hour< 19)
+            {
+                Console.WriteLine("Good Evening!");
+            }
+            else
+            {
+                Console.WriteLine("Good Night!");
+            }
+
+
+        }
+    }
+}
---
+++ Week2Challenges/Week2Challenges.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Challenges
+{
+    class Week2Challenges
+    {
+        static void Main(string[] args)
+        {
+            Greeter test = new Greeter();
+            string name = "Connor";
+            test.SayHello(name);
+
+            test.SayFarewell(name);
+            test.TemporalMessage();
+        }
+    }
+}
